4c34da9fd6775865171cdcf0a3c4c64aa64f517b
udhaya <udhaya.dhanabalan@gmail.com>
2015-04-28 15:29:59 -0700

    changes in biograph field

--- db/migrate/20150414021712_create_creators.rb
+++ db/migrate/20150414021712_create_creators.rb
@@ -2,8 +2,8 @@
 class CreateCreators < ActiveRecord::Migration
   def change
     create_table :creators do |t|
-        t.string :reference_creator_id,:name, :slug, :biography,:user_updated_at, :user_created_at
-        t.text :backed_projects, :started_projects, :location, :urls, :avatar, :category_wheel,:kickstart_creator_url
+        t.string :reference_creator_id,:name, :slug,:user_updated_at, :user_created_at
+        t.text :backed_projects, :started_projects, :location, :urls, :avatar, :category_wheel,:kickstart_creator_url,:biography
         t.integer :started_projects_count, :unanswered_surveys_count, :backed_projects_count, :created_projects_count, :unread_messages_count
         t.boolean :notifiy, :social	
         
