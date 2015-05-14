51933950d88418f984f1dd132b20a28131fb6aa1
udhaya <udhaya.dhanabalan@gmail.com>
2015-05-13 17:13:26 -0700

    changes and added pledges graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,11 +7,34 @@ class HomeController < ApplicationController
 		
 		
 		def search
+				sql = ActiveRecord::Base.connection();
   		if(params[:searchs] && params[:searchs] != "")
 												@project = Project.find_by_kickstart_project_url(params[:searchs])
-												@project_pledged_backers = @project.pledged_backers if @project
-						
+												if @project
+														#Pledged data
+																		@project_pledged_backers = PledgedBacker.find_by_sql("select pledged,backers_count,DATE(CONVERT_TZ( pledges_created_at ,  'US/Pacific',  'US/Mountain' )) as pledges_created_at from pledged_backers where project_id = '#{@project.id}'")
+																		#~ @pledges = sql.execute("SELECT pledged, DATE(pledges_created_at) FROM pledged_backers where project_id = '#{@project.id}' GROUP BY DATE(pledged_backers.pledges_created_at);") rescue {}
+																		#~ @pledges = sql.execute("SELECT min(pledged),max(pledged), DATE(pledges_created_at) FROM pledged_backers where project_id = '#{@project.id}' GROUP BY DATE(pledged_backers.pledges_created_at);") rescue {}
+																		@pledges = sql.execute("SELECT min(pledged),max(pledged), DATE(CONVERT_TZ( pledges_created_at ,  'US/Pacific',  'US/Mountain' )) FROM pledged_backers where project_id = '#{@project.id}' GROUP BY  DATE(CONVERT_TZ(pledged_backers.pledges_created_at ,  'US/Pacific',  'US/Mountain' ));") rescue {}
+																		@pledges_graph = @pledges.each_with_index.collect { |item,index| 
+																		puts "ddddddddddddddddddddddddddddddddddddd #{index}"
+																		if index == 0
+																		{:date => item[2], :value => (item[1].to_i).to_s} 
+																		else
+																		{:date => item[2], :value => (item[1].to_i-item[0].to_i).to_s} 
+																		end
+																		}
+																				@goal_date_count = ((DateTime.strptime(@project.deadline,'%s').to_date - @pledges_graph.last[:date]).to_i - 1)
+																				for i in 1..@goal_date_count
+																						@pledges_graph << {:date =>(@pledges_graph.last[:date] + 1), :value => ""}
+																				end
+																		#~ @pledges_graph_end = {:date =>DateTime.strptime(@project.deadline,'%s').in_time_zone("Pacific Time (US & Canada)").to_date, :value => ""}
+																		#~ @pledges_graph<< @pledges_graph_end
+																		@full_pledge_graph = @pledges_graph.to_json
+																		
+								end
 				end
+										
 				
 				if  (params[:searchs] && @project.blank?)
 								flash[:notice] = 'Invalid Url'
